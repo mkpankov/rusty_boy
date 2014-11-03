7ee7568a7d9044e9f66dcc0790f94b4740e467ae
Michael Pankov <constantius9@gmail.com>
2014-11-04 00:33:32 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate time;
+
+use std::io;
+use time::get_time;
+
+
+fn main() {
+    loop {
+
+        let t_comp  = time::precise_time_ns();
+        let slack   = 500000000;
+        println!("I'm gonna shoot you in {}, at {}!", slack, t_comp + slack);
+
+        let r = io::stdio::stdin().read_char();
+        let t_human  = time::precise_time_ns();
+        println!("Your time: {}, that was in {}", t_human, t_human - t_comp);
+
+        if t_comp + slack > t_human {
+            println!("You shot me!")
+        } else {
+            println!("I shot you.")
+        }
+
+        match r {
+            Ok(_) => continue,
+            Err(_) => break,
+        }
+
+    }
+}
