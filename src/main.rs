769fda67b87f2a123cb77467cf52b0063cb10097
Michael Pankov <constantius9@gmail.com>
2014-11-04 01:09:34 +0300

    Add draw call

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 extern crate time;
 
 use std::io;
+use std::io::Timer;
+use std::time::Duration;
 use std::num::pow;
-use time::get_time;
+use std::rand::distributions::{IndependentSample, Range};
+
+use time::precise_time_ns;
 
 
 fn main() {
     loop {
+        let range = Range::new(1000 * pow(10, 6), 1500 * pow (10, 6));
+        let mut rng = std::rand::task_rng();
+        let draw_delay_offset : u64 = range.ind_sample(&mut rng);
+        let draw_delay = 500 * pow(10, 6) + draw_delay_offset;
+
+        println!("Setting timer for {}", draw_delay.to_i64().unwrap());
+        let mut timer = Timer::new().unwrap();
+        let timeout = timer.oneshot(
+            Duration::nanoseconds(draw_delay.to_i64().unwrap()));
+
+        timeout.recv();
+        println!("Draw!");
 
         let t_comp  = time::precise_time_ns();
         let slack   = 500 * pow(10, 6);
