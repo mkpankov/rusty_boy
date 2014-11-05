931af5475fb784a8b54814708ef24cbb0a1d2a73
Michael Pankov <constantius9@gmail.com>
2014-11-05 23:29:01 +0300

    First working guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,37 @@
-extern crate time;
-
 use std::io;
-use std::io::Timer;
-use std::time::Duration;
-use std::num::pow;
-use std::rand::distributions::{IndependentSample, Range};
-
-use time::precise_time_ns;
-
+use std::rand;
 
 fn main() {
     loop {
-        let range = Range::new(1000 * pow(10, 6), 1500 * pow (10, 6));
-        let mut rng = std::rand::task_rng();
-        let draw_delay_offset : u64 = range.ind_sample(&mut rng);
-        let draw_delay = 500 * pow(10, 6) + draw_delay_offset;
+        let a = rand::random::<int>();
+        let b = rand::random::<int>();
 
-        println!("Setting timer for {}", draw_delay.to_i64().unwrap());
-        let mut timer = Timer::new().unwrap();
-        let timeout = timer.oneshot(
-            Duration::nanoseconds(draw_delay.to_i64().unwrap()));
+        println!("Solve this: {} + {} = ?", a, b)
 
-        timeout.recv();
-        println!("Draw!");
+        let result = io::stdio::stdin().read_line();
 
-        let t_comp  = time::precise_time_ns();
-        let slack   = 500 * pow(10, 6);
-        println!("I'm gonna shoot you in {}, at {}!", slack, t_comp + slack);
-
-        let r = io::stdio::stdin().read_char();
-        let t_human  = time::precise_time_ns();
-        println!("Your time: {}, that was in {}", t_human, t_human - t_comp);
-
-        if t_comp + slack > t_human {
-            println!("You shot me!")
-        } else {
-            println!("I shot you.")
-        }
-
-        match r {
-            Ok(_) => continue,
+        match result {
+            Ok(mut string) => {
+                let n = string.len() - 1;
+                string.remove(n);
+                let maybe_c_user : Option<int> =
+                    std::from_str::from_str(string.as_slice());
+                match maybe_c_user {
+                    Some(c_user) => {
+                        let message =
+                            if c_user == a + b {
+                                "Correct!"
+                            } else {
+                                "Incorrect!"
+                            };
+                        println!("{}", message);
+                    },
+                    None => {
+                        println!("You didn't input a number. Try again.");
+                    },
+                }
+            },
             Err(_) => break,
-        }
-
+        };
     }
 }
